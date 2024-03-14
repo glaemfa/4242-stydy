6f2b5a3feb828bdec449d55c248edbc15bd2a8f6
glaemfa <160819065+glaemfa@users.noreply.github.com>
2024-03-14 18:17:28 +0900

    Add files via upload

---
+++ week03/2박.js
@@ -0,0 +1,20 @@
+    var box1_value=prompt("첫 번째 값을 입력해주세요.")
+    document.            ('.box1').textContent=       ;
+
+    var box2_value=prompt("두 번째 값을 입력해주세요.")
+    document.            ('.box2').textContent=       ;
+
+    var box3_value=prompt("세 번째 값을 입력해주세요.")
+    document.            ('.box3').textContent=       ;
+
+function Comparison(){
+
+    var box1_value=     (document.querySelector('.box1').textContent);
+    var box2_value=     (document.querySelector('.box2').textContent);
+    var box3_value=     (document.querySelector('.box3').textContent);
+
+    var max,min;
+    
+
+
+}
